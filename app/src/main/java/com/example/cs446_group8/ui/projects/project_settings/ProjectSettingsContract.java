e3ec23ad01e594b136b40b6745a59b52f408dc49
Ajay Mistry <mis.try.ajay.r@gmail.com>
2020-07-28 21:16:17 -0400

    Adding more frontend logic to project settings

--- app/src/main/java/com/example/cs446_group8/ui/projects/project_settings/ProjectSettingsContract.java
+++ app/src/main/java/com/example/cs446_group8/ui/projects/project_settings/ProjectSettingsContract.java
@@ -1,7 +1,5 @@
 package com.example.cs446_group8.ui.projects.project_settings;
 
-import android.view.View;
-
 import com.example.cs446_group8.ui.BaseContract;
 
 public interface ProjectSettingsContract extends BaseContract {
